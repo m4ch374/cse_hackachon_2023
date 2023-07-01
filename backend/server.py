00a58c3eabfd01e5ed5dd9851ea8c2dc717cf6ed
amith-laptop <kovoor71@gmail.com>
2023-07-01 15:56:34 +1000

    started flask-ing server

--- backend/server.py
+++ backend/server.py
@@ -1,2 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+
+APP = Flask(__name__, static_url_path='/static/')
+CORS(APP)
+
+APP.config['TRAP_HTTP_EXCEPTIONS'] = True
+APP.register_error_handler(Exception, defaultHandler)
+
 if __name__ == "__main__":
     print("This is the server")
